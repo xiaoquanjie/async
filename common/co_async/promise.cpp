fa81e9a654ac5ce46d4bdee8283032fd41159ca1
wujuju <heyang87912@qq.com>
2022-07-01 13:06:26 +0800

    引久promise机制

---
+++ common/co_async/promise.cpp
@@ -0,0 +1,155 @@
+#include "common/co_async/promise.h"
+#include "common/co_async/time_pool.h"
+#include "common/coroutine/coroutine.hpp"
+#include <assert.h>
+#include <unordered_set>
+
+namespace co_async {
+
+TimerPool g_time_pool;
+int64_t g_unique_id = 1;
+std::unordered_set<int64_t> g_unique_id_set;
+
+int64_t genUniqueId() {
+    return g_unique_id++;
+}
+
+void addUniqueId(int64_t id) {
+    assert(g_unique_id_set.find(id) == g_unique_id_set.end());
+    g_unique_id_set.insert(id);
+}
+
+bool rmUniqueId(int64_t id) {
+    auto iter = g_unique_id_set.find(id);
+    if (iter != g_unique_id_set.end()) {
+        g_unique_id_set.erase(iter);
+        return true;
+    }
+    return false;
+}
+
+int64_t addTimer(int interval, std::function<void()> func) {
+    return g_time_pool.AddTimer(interval, func);
+}
+
+void rmTimer(int64_t timer_id) {
+    g_time_pool.CancelTimer(timer_id);
+}
+
+bool loop(uint32_t cur_time) {
+    g_time_pool.Update();
+    return true;
+}
+
+int wait(uint32_t interval) {
+    unsigned int co_id = Coroutine::curid();
+    if (co_id == M_MAIN_COROUTINE_ID) {
+        assert(false);
+        return E_ERROR;
+    }
+
+    addTimer(interval, [co_id]() {
+        Coroutine::resume(co_id);
+    });
+
+    Coroutine::yield();
+    return E_OK;
+}
+
+void Resolve::operator()(std::shared_ptr<void> res) const {
+    if (!rmUniqueId(this->result->uniqueId)) {
+        //printf("resolve\n");
+        return;
+    }
+
+    this->result->resolveRes = res;
+
+    if (this->result->timerId != 0) {
+        rmTimer(this->result->timerId);
+        Coroutine::resume(this->result->coId);
+    }
+}
+
+void Reject::operator()(int err, const std::string& msg) const {
+    if (!rmUniqueId(this->result->uniqueId)) {
+        //printf("reject\n");
+        return;
+    }
+
+    this->result->rejectRes = std::make_shared<PromiseReject>();
+    this->result->rejectRes->err = err;
+    this->result->rejectRes->msg = msg;
+
+    if (this->result->timerId != 0) {
+        rmTimer(this->result->timerId);
+        Coroutine::resume(this->result->coId);
+    }
+}
+
+std::pair<int, std::shared_ptr<void>> promise(std::function<void(Resolve, Reject)> fn, int timeOut) {
+    auto promiseNull = std::make_shared<PromiseNull>();
+    std::pair<int, std::shared_ptr<void>> ret = std::make_pair(E_ERROR, promiseNull);
+    
+    unsigned int coId = Coroutine::curid();
+    if (coId == M_MAIN_COROUTINE_ID) {
+        //assert(false);
+        return ret;
+    }
+
+    // 生成一个唯一id
+    int64_t uniqueId = genUniqueId();
+
+    // 添加唯一值进去
+    addUniqueId(uniqueId);
+
+    auto result = std::make_shared<PromiseResult>();
+    result->coId = coId;
+    result->uniqueId = uniqueId;
+
+    Resolve resolve;
+    resolve.result= result;
+    Reject reject;
+    reject.result = result;
+
+    // 执行异步
+    fn(resolve, reject);
+
+    // 如果立马有了结果，则说明没有异步
+    if (!result->resolveRes && !result->rejectRes) {
+        // 添加定时器
+        int64_t timerId = addTimer(timeOut, [result]() {
+            if (!rmUniqueId(result->uniqueId)) {
+                //printf("timeout\n");
+                return;
+            }
+            result->timeOutFlag = true;
+            Coroutine::resume(result->coId);
+        });
+
+        result->timerId = timerId;
+        // 挂起协程
+        Coroutine::yield();
+    }
+
+    if (result->timeOutFlag) {
+        ret.first = E_TIMEOUT;
+    }
+    else {
+        if (result->resolveRes) {
+            ret.first = E_OK;
+            ret.second = result->resolveRes;
+        }
+        else {
+            ret.first = E_ERROR;
+            ret.second = result->rejectRes;
+        }
+    }
+
+    return ret;
+}
+
+void setTimeout(std::function<void()> fn, int timeOut) {
+    addTimer(timeOut, fn);
+}
+
+}
---
+++ common/co_async/promise.h
@@ -0,0 +1,109 @@
+/*----------------------------------------------------------------
+// Copyright 2022
+// All rights reserved.
+//
+// author: 404558965@qq.com (xiaoquanjie)
+//----------------------------------------------------------------*/
+
+/*
+ * 
+*/
+
+#pragma once
+
+#include <functional>
+#include <memory>
+
+namespace co_async {
+
+// promise的返回值
+enum {
+    // 访问成功
+    E_OK = 0,
+    // 访问超时
+    E_TIMEOUT = 1,
+    // 访问失败, 
+    E_ERROR = 2
+};
+
+struct PromiseNull {};
+
+struct PromiseReject {
+    int err = 0;
+    std::string msg;
+};
+
+struct PromiseResult {
+    bool timeOutFlag = false;
+    int64_t timerId = 0;
+    int64_t uniqueId = 0;
+    uint32_t coId = 0;
+    std::shared_ptr<void> resolveRes;
+    std::shared_ptr<PromiseReject> rejectRes;
+};
+
+struct Resolve {
+    std::shared_ptr<PromiseResult> result;
+    void operator()(std::shared_ptr<void> res) const;
+};
+
+struct Reject {
+    std::shared_ptr<PromiseResult> result;
+    void operator()(int err, const std::string& msg) const;
+};
+
+// 循环，内部调用异步循环
+bool loop(uint32_t cur_time = 0);
+
+// 毫秒
+int wait(uint32_t interval);
+
+// 返回值是pair: first=[E_OK/E_TIMEOUT/E_ERROR], second=[std::shared_ptr<PromiseNull>, other]
+// 1、first=E_OK时, second=目标
+// 2、first=E_TIMEOUT时，second=std::shared_ptr<PromiseNull>
+// 3、first=E_ERROR时，second=std::shared_ptr<PromiseReject>
+std::pair<int, std::shared_ptr<void>> promise(std::function<void(Resolve, Reject)> fn, int timeOut = 3000);
+
+void setTimeout(std::function<void()> fn, int timeOut);
+
+//////////////////////// // 辅助获取promise的值 
+
+inline bool checkOk(std::pair<int, std::shared_ptr<void>> p) {
+    return (p.first == E_OK);
+}
+
+inline bool checkTimeout(std::pair<int, std::shared_ptr<void>> p) {
+    return (p.first == E_TIMEOUT);
+}
+
+inline bool checkError(std::pair<int, std::shared_ptr<void>> p) {
+    return (p.first == E_ERROR);
+}
+
+template<typename T>
+inline std::shared_ptr<T> getOk(std::pair<int, std::shared_ptr<void>> p) {
+    if (checkOk(p)) {
+        auto res = std::static_pointer_cast<T>(p.second);
+        return res;
+    }
+    else {
+        return nullptr;
+    }
+}
+
+inline std::shared_ptr<PromiseReject> getError(std::pair<int, std::shared_ptr<void>> p) {
+    if (checkError(p)) {
+        auto res = std::static_pointer_cast<PromiseReject>(p.second);
+        return res;
+    }
+    else {
+        return nullptr;
+    }
+}
+
+//////////////////////// // 辅助获取promise的值 
+
+}
+
+
+

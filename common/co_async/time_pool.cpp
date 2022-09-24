6e08d7ba5ee7632c3f55a8b3c731fac6dca081e7
wujuju <heyang87912@qq.com>
2022-09-24 15:19:24 +0800

    时间池优化

--- common/co_async/time_pool.cpp
+++ common/co_async/time_pool.cpp
@@ -5,29 +5,45 @@
 // author: 404558965@qq.com (xiaoquanjie)
 //----------------------------------------------------------------*/
 
+#include "common/co_async/time_pool.h"
+#include "common/log.h"
 #include <map>
 #include <list>
 #include <string.h>
 #include <assert.h>
-#include "common/co_async/time_pool.h"
-
-#ifndef WIN32
-#else
-#endif
+#include <memory>
+#include <time.h>
 
 namespace co_async {
+namespace t_pool {
 
-#define M_TIME_POOL_PRINTF(...)
-//#define M_TIME_POOL_PRINTF printf
+struct TimeNode {
+    uint64_t timerId = 0;
+    uint64_t expire = 0;
+    std::function<void()> cb;
+};
+
+struct PoolInfo {
+    bool init = false;
+    uint32_t maxIntervalDays = 1;
+    void** bucket = nullptr;
+    uint32_t bigIter = 0;
+    uint32_t smallIter = 0;
+    uint64_t allocTimerId = 1;  // 分配的id
+    uint64_t begTime = 0;
+
+    ~PoolInfo();
+};
+
+typedef std::multimap<uint64_t, TimeNode> TimeNodeMulMap;
 
 // 大桶的精度是1秒
-static const int big_bucket_cnt = 24 * 3600;
+const uint32_t gBigBucket = 24 * 3600;
 // 小桶的精度是1/10秒
-static const int small_bucket_cnt = 10;
+const uint32_t gSmallBucket = 10;
+PoolInfo gPoolInfo;
 
-typedef std::multimap<unsigned long long, TimerPool::TimeNodePtr> TimeNodeMap;
-
-clock_t get_mil_clock() {
+clock_t getMilClock() {
 #ifndef WIN32
 	struct timespec ts;
 	clock_gettime(CLOCK_MONOTONIC, &ts);
@@ -37,186 +53,218 @@ clock_t get_mil_clock() {
 #endif
 }
 
-TimerPool::TimerPool() {
+// 释放
+PoolInfo::~PoolInfo() {
+    if (!this->bucket) {
+        return;
+    }
+
+    uint32_t totalBigBucket = gBigBucket * this->maxIntervalDays; 
+    for (uint32_t idx = 0; idx < totalBigBucket; ++idx) {
+        void** b = (void**)this->bucket[idx];
+        for (uint32_t idx2 = 0; idx2 < gSmallBucket; ++idx2) {
+            void* b2 = b[idx2];
+            if (b2) {
+                TimeNodeMulMap* p = (TimeNodeMulMap*)b2;
+			    delete (p);
+            }
+        }
+        free(b);
+    }
+
+    free(this->bucket);
 }
 
-TimerPool::~TimerPool() {
-	for (int idx = 0; idx < big_bucket_cnt * _max_interval_day; ++idx) {
-		for (int idx2 = 0; idx2 < small_bucket_cnt; ++idx2) {
-			void** pp = (void**)_bucket[idx];
-			TimeNodeMap* p = (TimeNodeMap*)pp[idx2];
-			delete (p);
-		}
-		free(_bucket[idx]);
-	}
-	free(_bucket);
+// 计算落在哪个桶
+bool calcBucket(uint64_t nowMil, uint32_t interval, uint32_t& bigBucket, uint32_t& smallBucket) {
+    if (nowMil < gPoolInfo.begTime) {
+        assert(false);
+        return false;
+    }
+
+    uint64_t intervalMil = nowMil - gPoolInfo.begTime + interval;
+    uint64_t intervalSec = intervalMil / 1000;
+
+    // 计算落在哪个大桶
+    bigBucket = intervalSec % (gBigBucket * gPoolInfo.maxIntervalDays);
+    // 计算落在哪个小桶
+    smallBucket = ((intervalMil % 1000) / 100) % 10;
+
+    return true;
 }
 
-void TimerPool::Init(int max_interval_day) {
-	if (_init) {
-		return;
-	}
+void setMaxInterval(uint32_t days) {
+    if (gPoolInfo.init) {
+        return;
+    }
+    if (days == 0) {
+        return;
+    }
+    if (days > 7) {
+        days = 7;
+    }
 
-	_init = true;
-	if (max_interval_day > 7 || max_interval_day < 0) {
-		max_interval_day = 1;
-	}
-	_max_interval_day = max_interval_day;
-
-	// 初始化大桶, 精度是1秒
-	_bucket = (void**)malloc(sizeof(void*) * big_bucket_cnt * _max_interval_day);
-	for (int idx = 0; idx < big_bucket_cnt * _max_interval_day; ++idx) {
-		// 初始化小桶
-		void** small_bucket_pointer = (void**)malloc(sizeof(void*) * small_bucket_cnt);
-		memset(small_bucket_pointer, 0, sizeof(void*) * small_bucket_cnt);
-		_bucket[idx] = (TimeNodeMap*)small_bucket_pointer;
-	}
-
-	_beg_time = get_mil_clock();
-	_big_bucket = 0;
-	_small_bucket = 0;
-	_cur_timer_id = 1;
+    gPoolInfo.maxIntervalDays = days;
 }
 
-void TimerPool::Update() {
-	Init();
+// 初始化
+void init() {
+    if (gPoolInfo.init) {
+        return;
+    }
 
-	int big_bucket = 0;
-	int small_bucket = 0;
-	unsigned long long now = get_mil_clock();
-	_CalcBucket(now, 0, big_bucket, small_bucket);
+    uint32_t totalBigBucket = gBigBucket * gPoolInfo.maxIntervalDays;
 
-	if (big_bucket < 0 || small_bucket < 0) {
-		return;
+    // 初始化大桶, 精度是1秒
+    gPoolInfo.bucket = (void**)malloc(sizeof(void*) * totalBigBucket);
+
+    for (uint32_t idx = 0; idx < totalBigBucket; ++idx) {
+        // 初始化小桶
+        void** small = (void**)malloc(sizeof(void*) * gSmallBucket);
+        memset(small, 0, sizeof(void*) * gSmallBucket);
+        gPoolInfo.bucket[idx] = small;
+    }
+
+    gPoolInfo.begTime = getMilClock();
+    gPoolInfo.init = true;
+}
+
+uint64_t addTimer(uint32_t interval, std::function<void()>func) {
+    init();
+
+    if (interval == 0) {
+        return 0;
+    }
+
+    uint32_t totalBigBucket = gBigBucket * gPoolInfo.maxIntervalDays;
+
+    if (interval > totalBigBucket * 1000) {
+		return 0;
 	}
 
-	// 桶轮循
-	std::list<TimeNodePtr> nodelist;
-	int s_idx = _big_bucket * 10 + _small_bucket;
-	int e_idx = big_bucket * 10 + small_bucket;
-	for (; s_idx <= e_idx; ++s_idx) {
-		int b = s_idx / 10;
-		int s = s_idx % 10;
-		void** pp = (void**)_bucket[b];
-		TimeNodeMap* pmap = (TimeNodeMap*)(pp[s]);
+    uint64_t nowMil = getMilClock();
+    uint32_t big = 0;
+    uint32_t small = 0;
+
+    if (!calcBucket(nowMil, interval, big, small)) {
+        return 0;
+    }
+
+    void** pb = (void**)gPoolInfo.bucket[big]; 
+	TimeNodeMulMap* pmap = (TimeNodeMulMap*)(pb[small]);
+	if (!pmap) {
+		pmap = new TimeNodeMulMap;
+		pb[small] = pmap;
+	}
+
+    if (gPoolInfo.allocTimerId == 0xFFFFFFFE) {
+		gPoolInfo.allocTimerId = 1;
+	}
+
+    uint64_t timerId = (big * 10 + small);
+	timerId = timerId << 32;
+	timerId += (gPoolInfo.allocTimerId++);
+
+    TimeNode node;
+	node.expire = nowMil + interval;
+	node.cb = func;
+	node.timerId = timerId;
+	pmap->insert(std::make_pair(node.expire, node));
+
+    log("add timer bigIter:%d, smallIter:%d, timerId:%llu, sysBigIter:%d, sysSmallIter:%d, expire:%llu, nowMil:%llu\n",
+		big, small, timerId, gPoolInfo.bigIter, gPoolInfo.smallIter, node.expire, nowMil);
+
+    return timerId;
+}
+
+bool cancelTimer(uint64_t id) {
+    uint32_t high32Bit = (id >> 32);
+    uint32_t big = high32Bit / 10;
+    uint32_t small = high32Bit % 10;
+
+    log("cancel timer bigIter:%d, smallIter:%d, timerId:%llu", big, small, id);
+
+    if (big >= gBigBucket * gPoolInfo.maxIntervalDays) {
+        assert(false);
+        return false;
+    }
+    if (small >= 10) {
+        assert(false);
+        return false;
+    }
+
+    void** pb = (void**)gPoolInfo.bucket[big];
+    TimeNodeMulMap* pmap = (TimeNodeMulMap*)(pb[small]);
+    if (!pmap) {
+        assert(false);
+        return false;
+    }
+
+    for (auto iter = pmap->begin(); iter != pmap->end(); ) {
+        if (iter->second.timerId == id) {
+            pmap->erase(iter);
+            if (pmap->empty()) {
+                delete pmap;
+                pb[small] = 0;
+            }
+            return true;
+        }
+        else {
+            ++iter;
+        }
+    }
+
+    return false;
+}
+
+bool update() {
+    if (!gPoolInfo.init) {
+        return false;
+    }
+
+    uint64_t nowMil = getMilClock();
+    uint32_t big = 0, small = 0;
+    if (!calcBucket(nowMil, 0, big, small)) {
+        return false;
+    }
+
+    int startIter = gPoolInfo.bigIter * 10 + gPoolInfo.smallIter;
+	int endIter = big * 10 + small;
+
+    std::list<TimeNode> nodeList;
+
+    for (; startIter <= endIter; ++startIter) {
+		int b = startIter / 10;
+		int s = startIter % 10;
+		void** pb = (void**)gPoolInfo.bucket[b];
+		TimeNodeMulMap* pmap = (TimeNodeMulMap*)(pb[s]);
 		if (!pmap) {
 			continue;
 		}
 		for (auto iter = pmap->begin(); iter != pmap->end();) {
-			if (iter->first <= now) {
-				nodelist.push_back(iter->second);
+			if (iter->first <= nowMil) {
+				nodeList.push_back(iter->second);
 				pmap->erase(iter++);
 			}
 			else {
 				++iter;
 			}
 		}
-		for (auto iter = nodelist.begin(); iter != nodelist.end(); ++iter) {
+		for (auto iter = nodeList.begin(); iter != nodeList.end(); ++iter) {
 			// callback
-			(*iter)->cb();
+			(*iter).cb();
 		}
-		nodelist.clear();
+		nodeList.clear();
 		if (pmap->empty()) {
 			delete pmap;
-			pp[s] = 0;
+			pb[s] = 0;
 		}
 	}
-	_big_bucket = big_bucket;
-	_small_bucket = small_bucket;
+
+    gPoolInfo.bigIter = big;
+	gPoolInfo.smallIter = small;
+    return true;
 }
 
-unsigned long long TimerPool::AddTimer(int interval, std::function<void()>func) {
-	Init();
-
-	if (interval <= 0) {
-		return 0;
-	}
-	if (interval > big_bucket_cnt * _max_interval_day * 1000) {
-		return 0;
-	}
-
-	unsigned long long now_mil = get_mil_clock();
-
-	int big_bucket = 0;
-	int small_bucket = 0;
-	_CalcBucket(now_mil, interval, big_bucket, small_bucket);
-
-	if (big_bucket < 0 || small_bucket < 0) {
-		return 0;
-	}
-
-	void** pp = (void**)_bucket[big_bucket];
-	TimeNodeMap* pmap = (TimeNodeMap*)(pp[small_bucket]);
-	if (!pmap) {
-		pmap = new TimeNodeMap;
-		pp[small_bucket] = pmap;
-	}
-	if (_cur_timer_id == 0xFFFFFFFE) {
-		_cur_timer_id = 1;
-	}
-
-	unsigned long long timer_id = (big_bucket * 10 + small_bucket);
-	timer_id = timer_id << 32;
-	timer_id += _cur_timer_id++;
-
-	auto node = std::make_shared<TimeNode>();
-	node->expire = now_mil + interval;
-	node->cb = func;
-	node->timer_id = timer_id;
-	pmap->insert(std::make_pair(node->expire, node));
-	M_TIME_POOL_PRINTF("add timer big_bucket:%d, small_bucket:%d, timer_id:%llu, sys_big_bucket:%d, sys_small_bucket:%d, expire:%llu, now_mil:%llu\n",
-		big_bucket, small_bucket, timer_id, _big_bucket, _small_bucket, node->expire, now_mil);
-	return timer_id;
 }
-
-int TimerPool::CancelTimer(unsigned long long id) {
-	unsigned long long flag = 0xFFFFFFFF;
-	/*int low_32bit = (int)(id & flag);*/
-	int high_32bit = (id >> 32) & flag;
-
-	int big_bucket = high_32bit / 10;
-	int small_bucket = high_32bit % 10;
-
-	M_TIME_POOL_PRINTF("cancel timer big_bucket:%d, small_bucket:%d, timer_id:%llu, sys_big_bucket:%d, sys_small_bucket:%d\n", 
-		big_bucket, small_bucket, id, _big_bucket, _small_bucket);
-	void** pp = (void**)_bucket[big_bucket];
-	TimeNodeMap* pmap = (TimeNodeMap*)(pp[small_bucket]);
-	if (!pmap) {
-		printf("cancel timer big_bucket:%d, small_bucket:%d, timer_id:%llu, sys_big_bucket:%d, sys_small_bucket:%d\n", 
-			big_bucket, small_bucket, id, _big_bucket, _small_bucket);
-		assert(false);
-		return -1;
-	}
-	for (auto iter = pmap->begin(); iter != pmap->end();) {
-		if (iter->second->timer_id == id) {
-			pmap->erase(iter);
-			if (pmap->empty()) {
-				delete pmap;
-				pp[small_bucket] = 0;
-			}
-			return 0;
-		}
-		else {
-			++iter;
-		}
-	}
-	//assert(false);
-	return -1;
-}
-
-bool TimerPool::_CalcBucket(unsigned long long now_mil, int interval, int& big_bucket, int& small_bucket) {
-	unsigned long long inteval_mil = (now_mil - _beg_time + interval);
-	//assert(inteval_mil > 0);
-	int interval_sec = inteval_mil / 1000;
-
-	big_bucket = interval_sec % (big_bucket_cnt * _max_interval_day);
-	small_bucket = ((inteval_mil % 1000) / 100) % 10;
-
-	if (big_bucket < 0 || small_bucket < 0 || now_mil < _beg_time) {
-		M_TIME_POOL_PRINTF("now_mil:%llu, _beg_time:%llu\n", now_mil, _beg_time);
-		assert(false);
-	}
-	return true;
-}
-
 }

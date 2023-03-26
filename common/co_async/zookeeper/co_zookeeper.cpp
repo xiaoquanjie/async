e9923eabee6ed79badcd0c36c73190bf9ccdd4cb
xiaoquanjie <404558965@qq.com>
2023-03-26 18:17:36 +0800

    引入协程版本zookeeper

---
+++ common/co_async/zookeeper/co_zookeeper.cpp
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------
+// Copyright 2023
+// All rights reserved.
+//
+// author: 404558965@qq.com (xiaoquanjie)
+// github: https://github.com/xiaoquanjie/async
+//----------------------------------------------------------------*/
+
+#ifdef USE_ASYNC_ZOOKEEPER
+
+#include "common/co_async/zookeeper/co_zookeeper.h"
+#include "common/co_async/promise.h"
+
+namespace co_async {
+namespace zookeeper {
+
+std::pair<int, async::zookeeper::ZookParserPtr> execute(const std::string& uri, std::shared_ptr<async::zookeeper::BaseZookCmd> cmd, const TimeOut& t) {
+    auto res = co_async::promise([&uri, cmd](co_async::Resolve resolve, co_async::Reject reject) {
+        async::zookeeper::execute(uri, cmd, [resolve](async::zookeeper::ZookParserPtr parser) {
+            resolve(parser);
+        });
+    }, t());
+
+    std::pair<int, async::zookeeper::ZookParserPtr> ret = std::make_pair(res.first, nullptr);
+    if (co_async::checkOk(res)) {
+        ret.second = co_async::getOk<async::zookeeper::ZookParser>(res);
+    } 
+    return ret;
+}
+
+std::pair<int, bool> execute2(const std::string& uri, std::shared_ptr<async::zookeeper::BaseZookCmd> cmd, const TimeOut& t) {
+    auto res = execute(uri, cmd, t);
+    std::pair<int, bool> ret = std::make_pair(res.first, false);
+
+    if (co_async::checkOk(res)) {
+        auto parser = res.second;
+        ret.second = parser->zOk();
+    }
+
+    return ret;
+}
+
+}
+}
+#endif
---
+++ common/co_async/zookeeper/co_zookeeper.h
@@ -0,0 +1,24 @@
+/*----------------------------------------------------------------
+// Copyright 2023
+// All rights reserved.
+//
+// author: 404558965@qq.com (xiaoquanjie)
+// github: https://github.com/xiaoquanjie/async
+//----------------------------------------------------------------*/
+
+#pragma once
+
+#include "common/async/zookeeper/async_zookeeper.h"
+#include "common/co_async/comm.hpp"
+
+namespace co_async {
+namespace zookeeper {
+
+// pair.first表示协程是否成功
+std::pair<int, async::zookeeper::ZookParserPtr> execute(const std::string& uri, std::shared_ptr<async::zookeeper::BaseZookCmd> cmd, const TimeOut& t = TimeOut());
+
+// pair.second表示命令执行是否成功
+std::pair<int, bool> execute2(const std::string& uri, std::shared_ptr<async::zookeeper::BaseZookCmd> cmd, const TimeOut& t = TimeOut());
+
+}
+}
